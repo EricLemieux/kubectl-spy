cb643700a208e49bb64e807b7a63e4577d6a8789
Eric Lemieux <eric@lemieuxdev.com>
2020-02-22 13:34:15 -0500

    Adding commander to process arguments

--- src/kubectl-spy.js
+++ src/kubectl-spy.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const program = require('commander');
+
+program
+    .version('0.1.0')
+    .option('-n, --namespace <string>', 'namespace for the secrets')
+    .parse(process.argv);
+
+console.log(`namespace ${program.namespace}`);
+program.args.forEach(secret => {
+    console.log(secret);
+});
