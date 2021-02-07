132eecfed27ed480ef22785f70fa97fad09e08cd
Eric Lemieux <eric@lemieuxdev.com>
2021-02-07 15:07:07 -0500

    Helpful error when a secret is not found
    Also refactors the end to end tests to assert that something should
    fail.

--- src/kubernetes.rs
+++ src/kubernetes.rs
@@ -1,11 +1,18 @@
-use kube::{
-    Api,
-    Client
-};
 use k8s_openapi::api::core::v1::Secret;
+use kube::{Api, Client};
+use std::process::exit;
 
 pub async fn get_secret(client: Client, secret_name: String, namespace: String) -> Secret {
-    let secrets :Api<Secret> = Api::namespaced(client, &namespace);
+    let secrets: Api<Secret> = Api::namespaced(client, &namespace);
 
-    return secrets.get(secret_name.as_str()).await.unwrap();
+    return match secrets.get(secret_name.as_str()).await {
+        Ok(secret) => secret,
+        Err(_e) => {
+            eprintln!(
+                "Unable to locate secret {:?} in namespace {:?}",
+                secret_name, namespace
+            );
+            exit(1);
+        }
+    };
 }
